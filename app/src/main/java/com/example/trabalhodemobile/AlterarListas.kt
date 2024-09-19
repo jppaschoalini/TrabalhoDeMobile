a0a40bdfde1524dab5e93d6ab949585be01cf3d7
jppaschoalini <joao.paschoalini@sou.unaerp.edu.br>
2024-09-19 19:22:03 -0300

    paschal

---
+++ app/src/main/java/com/example/trabalhodemobile/AlterarListas.kt
@@ -0,0 +1,24 @@
+package com.example.trabalhodemobile
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AlterarListas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_itens)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        val Button: Button = findViewById(R.id.adicionar)
+    }
+}
