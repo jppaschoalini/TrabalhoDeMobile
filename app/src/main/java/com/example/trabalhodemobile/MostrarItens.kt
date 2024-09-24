1e9199bbccf346360fab94638d31a97c782dc71d
jppaschoalini <joao.paschoalini@sou.unaerp.edu.br>
2024-09-24 13:01:59 -0300

    paschal

---
+++ app/src/main/java/com/example/trabalhodemobile/MostrarItens.kt
@@ -0,0 +1,20 @@
+package com.example.trabalhodemobile
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MostrarItens : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_mostrar_itens)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
